92116f06152f63a2fcef5e0d082b73ccdcf825af
Brandon Holt <brandonholt95@gmail.com>
2021-10-11 17:36:56 -0400

    Update singleEquation.m

--- singleEquation.m
+++ singleEquation.m
@@ -1,16 +1,16 @@
 %constants
-Bmax = [50000];
+Bmax = 5e5;
 Km = 9e12;
+a = 1e-13;
+b = 2e-2;
 r = 3;
-kcat = [6956398505.51782];
-a = 1e-6;
-b = 2e5;
+kcat = 25e10;
 
 %system of equations
 f = @(t,x) [r*x(1)*(1 - (x(1)/Bmax)) - (a*x(1)*x(3));-kcat*x(1)*x(2)/(Km + x(2));(kcat*x(1)*x(2)/(Km + x(2)))- (b*x(1)*x(3))];
 %set interval and initial conditions
-int = [0 72]; %hours
-init = [500 2.4e16 0]; %5000 = 10^9 CFU/mL 1.2e16 = 100 uM drug
+int = [0 24]; %hours
+init = [500 4.8e14 0]; %500 bac/uL (1000X dil), %800 uM drug = 4.8e14 copies/uL
 options1 = odeset('Refine',4);
 options2 = odeset(options1,'NonNegative',1);
 [t,xa] = ode15s(f,int,init,options2);
